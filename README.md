b1c8ae15fbfd03e857229ecbbe1b9c805bb77750
floason <219914207+floason@users.noreply.github.com>
2025-12-15 00:00:33 +0000

    Set up repository

---
+++ README.md
@@ -0,0 +1,4 @@
+# Bulb
+Bulb is a chat protocol I have created. This repository allows you to compile both a client and server using the Bulb protocol.
+
+The client is a CLI.
---
+++ src/main.c
@@ -0,0 +1,12 @@
+// floason (C) 2025
+// Licensed under the MIT License.
+
+#include <stdio.h>
+#include "bulb_version.h"
+
+int main()
+{
+    puts("Hello world!");
+    printf("Version %d.%d.%d\n", MAJOR, MINOR, PATCH);
+    return 0;
+}
